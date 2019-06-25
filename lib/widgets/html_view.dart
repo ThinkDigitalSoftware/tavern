99cf3e2611d1e0106fb1d30bfd11f9827cd9fd0d
Jonathan White <jwhite@thinkdigital.software>
2019-06-24 19:38:51 -0700

    Add HtmlView widget and Feed class to retrieve the Atom Feed from pub.dev

---
+++ lib/widgets/html_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:html2md/html2md.dart' as html2md;
+
+class HtmlView extends StatelessWidget {
+  final String html;
+  final String markdown;
+
+  HtmlView({Key key, this.html})
+      : this.markdown = html2md.convert(html),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Markdown(
+      data: markdown,
+    );
+  }
+}
