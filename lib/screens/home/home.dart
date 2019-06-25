35f7e66f408d788261ce3f3442569f4d4f18511f
GroovinChip <groovinchip@gmail.com>
2019-06-25 11:16:38 -0400

    fleshing out list of packages page

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -9,6 +9,8 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   TextEditingController _searchController = TextEditingController();
+  PubClient _client = PubClient();
+  PubHtmlParsingClient _htmlParsingClient = PubHtmlParsingClient();
 
   @override
   Widget build(BuildContext context) {
@@ -17,36 +19,73 @@ class _HomeState extends State<Home> {
         backgroundColor: Color(0xFF122030),
         title: Image.asset('images/dart-packages-white.png'),
       ),
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverFloatingBar(
-            floating: true,
-            snap: true,
-            elevation: 4,
-            title: TextField(
-              controller: _searchController,
-              onChanged: (searchQuery) {},
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                hintText: 'Search Dart packages',
-              ),
-            ),
-            trailing: IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () async {
-                PubHtmlParsingClient client = PubHtmlParsingClient();
-                var result = await client.get(_searchController.text);
-                print(result);
-              }, //TODO: launch search with query
-            ),
-          ),
-          SliverList(delegate: SliverChildBuilderDelegate((context, index) {
-            //TODO: return 'Top Packages'
-            return ListTile(
-              title: Text('$index'),
+      body: FutureBuilder<Page>(
+        future: _client.getPageOfPackages(1),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(),
             );
-          })),
-        ],
+          } else {
+            final page = snapshot.data;
+            return CustomScrollView(
+              slivers: <Widget>[
+                SliverFloatingBar(
+                  floating: true,
+                  snap: true,
+                  elevation: 4,
+                  title: TextField(
+                    controller: _searchController,
+                    onChanged: (searchQuery) {},
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Search Dart packages',
+                    ),
+                  ),
+                  trailing: IconButton(
+                    icon: Icon(Icons.search),
+                    onPressed: () async {
+                      PubHtmlParsingClient client = PubHtmlParsingClient();
+                      var result = await client.get(_searchController.text);
+                      print(result);
+                    }, //TODO: launch search with query
+                  ),
+                ),
+                SliverList(delegate: SliverChildBuilderDelegate((context, index) {
+                  //TODO: return 'Top Packages'
+                  return FutureBuilder<FullPackage>(
+                    future: _htmlParsingClient.get(page.packages[index].name),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        FullPackage _package = snapshot.data;
+                        //print(_package.name + " " + _package.score.toString());
+                        return ListTile(
+                          title: Text(_package.name),
+                          trailing: CircleAvatar(
+                            child: Text(_package.score.toString()),
+                          ),
+                        );
+                      } else {
+                        return Container();
+                      }
+                    },
+                  );
+                },
+                childCount: page.packages.length
+                )),
+                /*SliverList(delegate: SliverChildListDelegate([
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text('Page 1'),
+                    ],
+                  ),
+                ])),*/
+
+              ],
+            );
+          }
+        },
       ),
     );
   }
