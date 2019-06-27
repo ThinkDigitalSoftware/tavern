bad000cba6e0d7e7b5f7893b49424c47ee275593
GroovinChip <groovinchip@gmail.com>
2019-06-27 10:46:00 -0400

    added Provider dependency for custom colors class, overhauled UI into a more modern style

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'screens/home/home.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(PubDevClientApp());
 
 class PubDevClientApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        fontFamily: 'Metropolis',
-        accentColor: Color(0xFF38bffc),
-        appBarTheme: AppBarTheme(
-          iconTheme: IconThemeData(color: Colors.white),
+    return Provider<PubColors>(
+      builder: (context) => PubColors(),
+      child: MaterialApp(
+        theme: ThemeData(
+          fontFamily: 'Metropolis',
+          accentColor: Color(0xFF38bffc),
+          appBarTheme: AppBarTheme(
+            iconTheme: IconThemeData(color: Colors.white),
+          ),
         ),
+        home: Home(),
+        debugShowCheckedModeBanner: false,
       ),
-      home: Home(),
-      debugShowCheckedModeBanner: false,
     );
   }
+}
+
+class PubColors {
+  Color darkColor = Color.fromRGBO(18, 32, 48, 1);
 }
