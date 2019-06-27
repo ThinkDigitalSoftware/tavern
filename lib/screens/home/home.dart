20fa66faf6b3d04b0fe252ab287d8f8295a28b1a
GroovinChip <groovinchip@gmail.com>
2019-06-27 09:06:52 -0400

    adjusted drawer headers text size
    refactored drawer into its own widget

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -6,6 +6,7 @@ import 'package:material_segmented_control/material_segmented_control.dart';
 import 'package:groovin_widgets/groovin_widgets.dart';
 import 'package:intl/intl.dart';
 import 'package:pub_dev_client/widgets/default_pacakges_list.dart';
+import 'package:pub_dev_client/widgets/main_drawer.dart';
 import 'package:pub_dev_client/widgets/pub_header.dart';
 
 class Home extends StatefulWidget {
@@ -40,127 +41,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       key: _scaffoldKey,
-      drawer: Drawer(
-        child: Column(
-          //padding: EdgeInsets.zero,
-          children: <Widget>[
-            //Get under the status bar without losing dark color
-            Container(
-              color: PubColors().darkColor,
-              height: 16,
-            ),
-            Container(
-              color: PubColors().darkColor,
-              child: ListTile(
-                leading: Icon(GroovinMaterialIcons.dart_logo, color: Colors.blue,),
-                title: Text(
-                  'pub.dev',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ),
-            Row(
-              //mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Text(
-                    'Getting Started:',
-                    style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            GroovinExpansionTile(
-              title: Text(
-                'Flutter:',
-                style: TextStyle(
-                  fontSize: 18,
-                ),
-              ),
-              children: <Widget>[
-                ListTile(
-                  title: Text('Using Packages'),
-                  trailing: Icon(Icons.launch),
-                  onTap: () {}, //TODO: launch https://flutter.dev/docs/development/packages-and-plugins/using-packages
-                ),
-                ListTile(
-                  title: Text('Developing Packages and Plugins'),
-                  trailing: Icon(Icons.launch),
-                  onTap: () {}, //TODO: launch https://flutter.dev/docs/development/packages-and-plugins/developing-packages
-                ),
-              ],
-            ),
-            GroovinExpansionTile(
-              title: Text(
-                'Web and Server:',
-                style: TextStyle(
-                  fontSize: 18,
-                ),
-              ),
-              children: <Widget>[
-                ListTile(
-                  title: Text('Using Packages'),
-                  trailing: Icon(Icons.launch),
-                  onTap: () {}, //TODO: launch hhttps://dart.dev/guides/packages
-                ),
-                ListTile(
-                  title: Text('Publishing a Package'),
-                  trailing: Icon(Icons.launch),
-                  onTap: () {}, //TODO: launch https://dart.dev/tools/pub/publishing
-                ),
-                ListTile(
-                  title: Text('Overview'),
-                  trailing: Icon(Icons.launch),
-                  onTap: () {}, //TODO: launch https://dart.dev/tools/pub/cmd
-                ),
-              ],
-            ),
-            Divider(
-              color: PubColors().darkColor,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 16, bottom: 8, top: 8),
-              child: Row(
-                children: <Widget>[
-                  Text(
-                    'Settings:',
-                    style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            ListTile(
-              title: Text(
-                'Toggle Dark Theme',
-                style: TextStyle(
-                  fontSize: 18,
-                ),
-              ), //TODO: change text based on which theme is on
-              trailing: Icon(Icons.brightness_3),
-              onTap: () {}, //TODO: toggle theme via DynamicTheme package
-            ),
-            Expanded(child: Container()),
-            Divider(
-              color: PubColors().darkColor,
-            ),
-            ListTile(
-              title: Text('Version 0.1.0'), //TODO: dynamically show version number based on build.gradle using package_info plugin
-              subtitle: Text('Authored by ThinkDigitalRepair and GroovinChip'),
-            ),
-          ],
-        ),
-      ),
+      drawer: MainDrawer(),
       body: FutureBuilder<Page>(
         future: _client.getPageOfPackages(1),
         builder: (context, snapshot) {
